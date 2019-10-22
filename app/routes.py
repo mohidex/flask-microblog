e455210fd69195de1527053326db27cc443b625e
Mohidul Islam <mohidul.nu@gmail.com>
2019-10-22 18:42:01 +0600

    Add a base.html

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Mohit'}
+    return render_template('index.html', title='Home', user=user)
