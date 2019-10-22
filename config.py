5160f2df6d4008fd00fc15d21bfb3452f35e4983
Mohidul Islam <mohidul.nu@gmail.com>
2019-10-23 01:15:44 +0600

    Added splite datebase

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-gess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    # disable the Flask-SQLAlchemy features that we don't need
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
