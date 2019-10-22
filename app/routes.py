7b7f31d01369b340db3b2f7851689d5e7aef7f6d
Mohidul Islam <mohidul.nu@gmail.com>
2019-10-23 00:34:33 +0600

    Added login form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -7,3 +8,12 @@ from app import app
 def index():
     user = {'username': 'Mohit'}
     return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
