661eee145465dbcd5f8031f998490760bc4ae2d5
Mohidul Islam <mohidul.nu@gmail.com>
2019-10-24 00:16:30 +0600

    Add profile section and edit profile

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from app import db
+from hashlib import md5
 from app import login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -16,12 +17,18 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
     def __repr__(self):
         return f'<User {self.username}>'
