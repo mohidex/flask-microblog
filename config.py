d624d0caadbfaf21538fb27adfb06ac02b77c9cd
Mohidul Islam <mohidul.nu@gmail.com>
2019-10-24 20:55:21 +0600

    Handles error and create log file

--- config.py
+++ config.py
@@ -9,3 +9,11 @@ class Config(object):
 
     # disable the Flask-SQLAlchemy features that we don't need
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # configure mail server
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['ifatmohit@gmail.com']
