187ffa1a17e0b2d9d67f7f98006b2f9960675472
Mohidul Islam <mohidul.nu@gmail.com>
2019-10-25 01:00:12 +0600

    Handles follow and unfollow functionallity

--- app/models.py
+++ app/models.py
@@ -44,6 +44,23 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_followed(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.join(followers, (followers.c.followed_id == Post.user_id))\
+            .filter(followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+
     def __repr__(self):
         return f'<User {self.username}>'
 
