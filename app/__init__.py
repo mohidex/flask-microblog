90078c65f4f1f0483558191a6c1cc9501c46675f
Mohidul Islam <mohidul.nu@gmail.com>
2019-10-26 18:11:01 +0600

    Improve the user interface

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
