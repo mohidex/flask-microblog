dc0abf1a0a4d66017d1ac56de02675a34474e815
Mohidul Islam <mohidul.nu@gmail.com>
2019-10-26 12:27:22 +0600

    SetUp email server

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,13 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+mail = Mail(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
