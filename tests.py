88496b923c2b7db57a440260d371aa1df376102f
Mohidul Islam <mohidul.nu@gmail.com>
2019-10-28 00:38:25 +0600

    Fixes the test cases :ok_hand:

--- tests.py
+++ tests.py
@@ -1,17 +1,26 @@
 import unittest
 from datetime import datetime, timedelta
-from app import app, db
+from app import db, create_app
 from app.models import User, Post
+from config import Config
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI =  'sqlite://'
 
 
 class UserModelCase(unittest.TestCase):
     def setUp(self) -> None:
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
         db.create_all()
 
     def tearDown(self) -> None:
         db.session.remove()
         db.drop_all()
+        self.app_context.pop()
 
     def test_password_hashing(self):
         u = User(username='susan')
