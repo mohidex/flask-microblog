00f194396a6accf4681ce79e7c551a8527c5f666
Mohidul Islam <mohidul.nu@gmail.com>
2019-11-01 00:29:33 +0600

    Add user popup template and view function

--- app/main/routes.py
+++ app/main/routes.py
@@ -138,3 +138,10 @@ def search():
         if page > 1 else None
     return render_template('search.html', title='Search', posts=posts,
                            next_url=next_url, prev_url=prev_url)
+
+
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user_popup.html', user=user)
